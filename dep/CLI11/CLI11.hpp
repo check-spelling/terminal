7415cb95f16f883642cf42147f45d927b367bbe2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-10-10 09:24:34 -0400

    Rewrite as sentences
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- dep/CLI11/CLI11.hpp
+++ dep/CLI11/CLI11.hpp
@@ -5471,7 +5471,7 @@ namespace CLI
         bool allow_extras_{ false };
 
         /// If ignore, allow extra arguments in the ini file (ie, don't throw an error). INHERITABLE
-        /// if error error on an extra argument, and if capture feed it to the app
+        /// If error, error on an extra argument. If capture, feed it to the app.
         config_extras_mode allow_config_extras_{ config_extras_mode::ignore };
 
         ///  If true, return immediately on an unrecognized option (implies allow_extras) INHERITABLE
